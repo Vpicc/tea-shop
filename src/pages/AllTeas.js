ce2bf85b6f70c8ead405bfa3f758008f2d1616c8
Victor Piccoli <victorpicc@gmail.com>
2019-06-12 21:15:51 -0300

    react way of listing

--- src/pages/AllTeas.js
+++ src/pages/AllTeas.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import TeaListing from '../components/TeaListing/TeaListing';
 
-const AllTeas = (props) => {
-    let array = [];
-    for(let i = 0; i < props.teaList.length; i++){
-        array.push(
-            <TeaListing key={props.teaList[i].id} teaName={ props.teaList[i].name } quantityInStock={props.teaList[i].stock_quantity} />
-        )
-    }
-
-    return(<ul className="list-group">{array}</ul>);
-};
+const AllTeas = (props) => (
+    <ul className="list-group">
+        {props.teaList.map((item,index) => (<TeaListing key={item.id} teaName={ item.name } quantityInStock={item.stock_quantity} />))}
+    </ul>
+);
 export default AllTeas;
