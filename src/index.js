735bc8cf9f86c0ad544a0ced116040870f6b1d32
Victor Piccoli <victorpicc@gmail.com>
2019-06-16 17:55:35 -0300

    removed unused dependencies

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap';
-import 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-const Root = () => (
-    <Router>
-        <App />
-    </Router>
-)
-
-ReactDOM.render(<Root />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
